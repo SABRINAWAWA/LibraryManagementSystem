258ced2a5c942acdfeb4e777e0707da3b8953515
adidkovs <adidkovs@mail.ccsf.edu>
2021-10-09 04:40:33 +0300

    update database

--- libraryproject/library/admin.py
+++ libraryproject/library/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Bookitems, LibraryMember
+from .models import Bookitems, Feedbacks, Librarian, LibraryMember, Rented_books, Reserved_books, hist_rented_books
 
 class BookitemsAdmin(admin.ModelAdmin):
     list_display=('title', 'authors', 'isbn')
@@ -10,3 +10,8 @@ class BookitemsAdmin(admin.ModelAdmin):
 
 admin.site.register(Bookitems, BookitemsAdmin)
 admin.site.register(LibraryMember)
+admin.site.register(Librarian)
+admin.site.register(Rented_books)
+admin.site.register(hist_rented_books)
+admin.site.register(Reserved_books)
+admin.site.register(Feedbacks)
