bd45105dfc386ebafadb43ec9ffefc7eabf4d08b
Huiying-Sabrina <53490593+SABRINAWAWA@users.noreply.github.com>
2021-11-30 13:13:01 -0800

    deployment errors fix 1

--- libraryproject/libraryproject/settings.py
+++ libraryproject/libraryproject/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'libraryproject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,'library', 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
