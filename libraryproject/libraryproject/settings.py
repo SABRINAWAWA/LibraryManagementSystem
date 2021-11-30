3054cc3169cc7a20572c1fc22a896b6eed589359
Huiying-Sabrina <53490593+SABRINAWAWA@users.noreply.github.com>
2021-11-29 16:45:48 -0800

    Settings changed

--- libraryproject/libraryproject/settings.py
+++ libraryproject/libraryproject/settings.py
@@ -10,11 +10,14 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 #import library
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# statis root : 
+STATIC_ROOT=os.path.join(BASE_DIR,"library","static")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
