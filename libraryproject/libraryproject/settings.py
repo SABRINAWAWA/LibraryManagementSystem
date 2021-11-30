7085a6255e0c9c29fa837f4eceb42cb9b872db69
Huiying-Sabrina <53490593+SABRINAWAWA@users.noreply.github.com>
2021-11-29 16:48:30 -0800

    Allowed_hosts added in the settings

--- libraryproject/libraryproject/settings.py
+++ libraryproject/libraryproject/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-0(ypmrfkrmz$m+re$f=z7iv_(&nu___63p@eln&7jjgej^8np_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["library-management-system-ccsf.herokuapp.com"]
 
 
 # Application definition
